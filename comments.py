b9739673948d57b82b9e2d8f1ad21c54176f3b4f
ASUS <biplobmojumder123456@gmail.com>
2026-05-03 02:31:48 +0600

    initial commmit

---
+++ comments.py
@@ -0,0 +1,6 @@
+print("Hello World")
+print("Whatsupp")
+"""
+This is Multiline code..
+for comments
+"""
---
+++ variables.py
@@ -0,0 +1,7 @@
+x = "This is variable"
+print(x)
+y = 5
+myname = "Biplob Mojumder"
+age = 24
+course_name = "data science"
+print(myname,age,course_name)
