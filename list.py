160110aab1c8ab99a51d27d85a0b987bd5183994
ASUS <biplobmojumder123456@gmail.com>
2026-05-13 02:29:08 +0600

    Refactor list.py to comment out snack list operations and add La Liga and Champions League examples

--- list.py
+++ list.py
@@ -1,4 +1,4 @@
-snaks = ["chips", "cookies", "candy"]
+"""snaks = ["chips", "cookies", "candy"]
 print(snaks[0])
 print(snaks[1]) 
 snaks.append("popcorn")  
@@ -11,4 +11,15 @@ print(len(snaks))
 print(snaks[2])
 print("hello world")
 id = 12345
-print(id)
+print(id)"""
+
+La_Liga =["Real madrid", "Barcelona", "Atletico Madrid", "Sevilla", "Valencia"]
+print(La_Liga)
+
+champions_league = La_Liga
+print(champions_league)
+champions_league.append("Psg")
+champions_league.append("Bayern Munich")
+champions_league.append("Manchester City")
+print(champions_league)
+champions_
