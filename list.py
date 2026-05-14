22ded6346cd273f517ad890e8ec198ef629b525f
ASUS <biplobmojumder123456@gmail.com>
2026-05-15 03:30:56 +0600

    Refactor list.py to comment out La Liga operations and enhance Europa League functionality with new append and pop operations

--- list.py
+++ list.py
@@ -13,10 +13,9 @@ print("hello world")
 id = 12345
 print(id)"""
 
-La_Liga =["Real madrid", "Barcelona", "Atletico Madrid", "Sevilla", "Valencia"]
-print(La_Liga)
-
-champions_league = La_Liga
+"""La_Liga =["Real madrid", "Barcelona", "Atletico Madrid", "Sevilla", "Valencia"]
+print(La_Liga)"""
+champions_league = ["Real madrid", "Barcelona", "Atletico Madrid", "Sevilla", "Valencia"]
 print(champions_league)
 champions_league.append("Psg")
 champions_league.append("Bayern Munich")
@@ -29,3 +28,10 @@ champions_league.pop(0)
 print(champions_league)
 champions_league.sort()
 print(champions_league)
+Europa_leauge = ["Chelsea", "Arsenal", "Manchester United", "Tottenham", "Liverpool"]
+#Europa_leauge= champions_league.copy()
+print(Europa_leauge)
+Europa_leauge.append("Napoli")
+print(Europa_leauge)
+Europa_leauge.pop(2)
+print(f"Europa League 2026: {Europa_leauge}")
